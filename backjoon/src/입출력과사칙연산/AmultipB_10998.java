54d7a132e8edf77782e6166417589fb3ab5d0422
Jaehee Kwon <kwun8746@gmail.com>
2022-02-10 17:40:54 +0900

    백준 알고리즘 풀이

---
+++ backjoon/src/입출력과사칙연산/AmultipB_10998.java
@@ -0,0 +1,16 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class AmultipB_10998 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+
+		System.out.println(a*b);
+
+	}
+
+}
